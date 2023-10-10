8683fe1f8792a833a3e66d59f62bc26d59c441fb
Kai Kreissner <Kai.Kreissner@googlemail.com>
2023-10-10 14:48:46 +0200

    Added comments to new class.

--- src/main/java/com/scitequest/martin/CustomIjKeyListener.java
+++ src/main/java/com/scitequest/martin/CustomIjKeyListener.java
@@ -3,19 +3,30 @@ package com.scitequest.martin;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * This class overrides the standard ImageJ KeyListener.
+ * Except a few exceptions all key events are removed by this.
+ */
 public class CustomIjKeyListener implements KeyListener {
 
-    private KeyListener ijKeyListener;
+    private final KeyListener ijKeyListener;
 
     public CustomIjKeyListener(KeyListener ijKeyListener) {
         this.ijKeyListener = ijKeyListener;
     }
 
+    /*
+     * This removes all keyTyped events.
+     */
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
 
+    /*
+     * This removes all keyPressed events except the ones specified in the
+     * if-statements below.
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
@@ -33,6 +44,9 @@ public class CustomIjKeyListener implements KeyListener {
         }
     }
 
+    /*
+     * This removes all keyReleased events.
+     */
     @Override
     public void keyReleased(KeyEvent e) {
     }
